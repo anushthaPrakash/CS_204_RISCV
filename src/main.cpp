2cd6954e1935b5b42625112d29b97c8f4da354de
Arpit Kumar Gautam <gautamarpit1204@gmail.com>
2023-03-13 05:01:33 +0530

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+/* 
+
+The project is developed as part of Computer Architecture class
+Project Name: Functional Simulator for subset of RISCV Processor
+
+Developer's Name:
+Developer's Email id:
+Date: 
+
+*/
+
+
+/* main.cpp 
+   Purpose of this file: The file handles the input and output, and
+   invokes the simulator
+*/
+
+
+// #include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include "myRISCVSim.h"
+using namespace std;
+
+int main()
+{
+  #ifndef ONLINE_JUDGE
+    freopen("output_log.mem", "w", stdout);
+  #endif
+  // reset the processor
+  reset_proc();
+  // load the program memory
+  load_program_memory();
+  // run the simulator
+  run_riscvsim();
+  return 0;
+}
